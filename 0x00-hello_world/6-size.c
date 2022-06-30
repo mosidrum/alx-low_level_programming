6e7f34ac0f0e9d2d6fdbdc23efeef4407c6c17db
mosidrum <mosiokanga@gmail.com>
2022-06-30 12:32:06 -0700

    debugging

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void)i
+int main(void)
 {
 printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
