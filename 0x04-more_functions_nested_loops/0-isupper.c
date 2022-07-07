33b33868c81a02d30888315554f9ab761e45691d
mosidrum <mosiokanga@gmail.com>
2022-07-07 18:25:07 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
+/** _isupper - checks the code
+*@c -  a passed in argument 
+* Return: _isupper is returned
+* Description: This program checks for upper or lower case alphabet 
+*/
+
 int _isupper(int c)
 {
 	char i;
