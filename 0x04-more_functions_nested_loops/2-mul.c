a45139fbd0f9431954693e3b0a1c4e174558e23a
mosidrum <mosiokanga@gmail.com>
2022-07-07 18:55:16 +0100

    multipliction

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Check the main file
+ * @a: An integer input value
+ * @b: Second integer input value
+ * Description: A function that multiplies two integers
+ * Return: Always 0
+ */
+ int mul(int a, int b)
+ {
+ 	int result;
+ 	result = a * b;
+
+ 	return (result);
+ }
+ 
