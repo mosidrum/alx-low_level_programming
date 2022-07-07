2d687e2b7849b8af5b9b20ca13c62f3c5b9da187
mosidrum <mosiokanga@gmail.com>
2022-07-07 19:06:38 +0100

    print numbers is done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* print_numbers: checks code
-* return: 0 always
+* print_numbers - a function that prints the numbers, from 0 to 9,
+* followed by a new line.
+* Return: Always 0
 */
 
 void print_numbers(void)
