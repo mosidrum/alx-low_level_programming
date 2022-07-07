d360337fb0a8c1bab6c52451d97de5e8c404e08a
mosidrum <mosiokanga@gmail.com>
2022-07-07 19:03:50 +0100

    print numbers is done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers: checks code
+* return: 0 always 
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
