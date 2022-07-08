2146640901b6e0f3ff0eb6cd3a1a0a478088055d
mosidrum <mosiokanga@gmail.com>
2022-07-08 05:26:18 +0100

    fixed all asap

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,29 @@
 #include "main.h"
 
 /**
-* print_square - Check the main file
-* @size: Value of square size
-* Description: A function that prints a square , followed by a new line
-* Return: 0
+* print_square - check for a digit
+* @n : number of _ to be printed
+* Return:void
 */
 
-void print_square(int size)
+void print_square(int n)
 {
-	int i;
 
-	if (size == 0 || size < 0)
-		_putchar('\n');
-	else
+	int i = 0, ii;
+
+	while (i < n && n > 0)
 	{
-		for (i = 0; i < size; i++)
+		ii = 0;
+		while (ii < n)
+		{
 			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
 	}
+	if (i == 0)
+		_putchar('\n');
+
 }
