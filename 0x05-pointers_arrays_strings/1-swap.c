43260b3aedd32ef79a000ef941e673763da6e6e9
mosidrum <mosiokanga@gmail.com>
2022-07-12 15:35:24 +0100

    swap numbers between two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int -> This swaps values of two integers
+* @a: parameter 1
+* @b: parameter 2 
+*/
+
+void swap_int(int *a, int *b)
+{
+	int keep;
+
+	keep = *a
+	a* = *b
+	*b = keep
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 1. 0-reset_to_98.c -> Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.
 
+2. 1-swap.c -> Write a function that swaps the values of two integers.
