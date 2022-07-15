5d8e8b0fe78ed698ed54faaac0743342ac4e1960
mosidrum <mosiokanga@gmail.com>
2022-07-15 01:50:38 +0100

    strcat implemented

--- 0x06-pointers_arrays_strings/strcat.c
+++ 0x06-pointers_arrays_strings/strcat.c
@@ -2,8 +2,8 @@
 
 /**
 * *_strcat -> This a function
-* @dest -> first parameter
-* @src -> second parameter
+* @dest: first parameter
+* @src: second parameter
 * Return: a string
 */
 char *_strcat(char *dest, char *src)
@@ -17,8 +17,8 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[len] = src[i];
-		len+=1;
+		len += 1;
 	}
 	dest[len] = '\0';
-	return(dest);
+	return (dest);
 }
