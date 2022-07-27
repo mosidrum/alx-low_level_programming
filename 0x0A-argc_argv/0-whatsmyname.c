b78cd98c25d234d53d4d498eb392e54c8c847908
mosidrum <mosiokanga@gmail.com>
2022-07-27 13:59:37 +0100

    program name succesffully printed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main -> This is  a program to prints its name
+*@argc: argc parameters
+*@argv: an array of commands listed
+*Return: 0 for success
+*/
+
+int main(argc, char argv[])
+{
+	printf("%s\n", argv[0])
+	return(0);
+}
