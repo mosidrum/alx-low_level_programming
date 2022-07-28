555d1e7036b35934bb42d79354952efaa6025f73
mosidrum <mosiokanga@gmail.com>
2022-07-28 02:49:49 +0100

    multipication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int result =0, num1, num2;
+	int result = 0, num1, num2;
 
-	if(argc != 3)
+	if (argc != 3)
 	{
 		printf("%s\n", "ERROR");
 		return (1);
 	}
-	num1 = atoi (argv[1]);
-	num2 = atoi (argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 	result = num1 * num2;
 
 	printf("%d\n", result);
-	return 0;
+	return (0);
 }
