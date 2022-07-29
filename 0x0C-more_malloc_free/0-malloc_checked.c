8c04e09dd81fb59482c63da54408a5cbe60f7d4c
mosidrum <mosiokanga@gmail.com>
2022-07-29 04:18:26 +0100

    malloc free

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,14 @@
 /**
 * *malloc_checked - checking malloc
 * @b: param b
+* Return: returns p
 */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = malloc(b);
+	p = malloc (b);
 	if (p == NULL)
 		exit(98);
 	return (p);
