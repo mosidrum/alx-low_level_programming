d3ce9cdc2d5108be1070aa133c9219f0499742a1
mosidrum <mosiokanga@gmail.com>
2022-08-02 18:31:34 +0100

    re coded

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,21 @@
 #define DOG_H
 
 /**
-* struct dog - a dog's basic info
-* @name: first member
-* @age: second member
-* @owner: third member
-*
-* Description: Longer description
-*/
-
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-char *name;
-char *owner;
-float age;
-}
+	char *name;
+	float age;
+	char *owner;
+};
 
 /**
-* dog_t - typedef for struct dog
-*/
-
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
-#endif
