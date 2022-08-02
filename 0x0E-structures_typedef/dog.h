81ccfc5a01da8a3e93c4e35b3c25b7693dea3a17
mosidrum <mosiokanga@gmail.com>
2022-08-02 18:29:59 +0100

    fixing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,24 +2,24 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: first member
- * @age: second member
- * @owner: third member
- *
- * Description: Longer description
- */
+* struct dog - a dog's basic info
+* @name: first member
+* @age: second member
+* @owner: third member
+*
+* Description: Longer description
+*/
 
 struct dog
 {
-    char *name;
-    char *owner;
-    float age;
+char *name;
+char *owner;
+float age;
 }
 
 /**
- * dog_t - typedef for struct dog
- */
+* dog_t - typedef for struct dog
+*/
 
 typedef struct dog dog_t;
 #endif
