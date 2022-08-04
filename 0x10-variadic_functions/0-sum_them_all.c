36fe0f4d520e580fc607627cd416517f7dcb9ee6
mosidrum <mosiokanga@gmail.com>
2022-08-04 17:42:15 +0100

    I tried this on my own

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,10 +19,9 @@ int sum_them_all(const unsigned int n, ...)
 	int i = 0, sum = 0;
 
 	for (i = 0; i < n; i++)
-		{
-			sum += va_arg(ap, unsigned int);
-		}
+	{
+		sum += va_arg(ap, unsigned int);
+	}
 	va_end(ap);
-	
 	return (sum);
 }
