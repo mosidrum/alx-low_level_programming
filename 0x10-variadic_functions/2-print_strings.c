6e8b2da67d87b90ac82d81421c4d595f973347f5
mosidrum <mosiokanga@gmail.com>
2022-08-05 07:06:13 +0100

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
-* print_numbers - prints numbers.
+* print_strings -  a function that prints strings
 * @separator: string to be printed between the strings
 * @n: number of strings passed to the function.
 *
