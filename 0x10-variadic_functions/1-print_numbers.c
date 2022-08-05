9640d32600abfb5391c1e37b75528569861c7bb7
mosidrum <mosiokanga@gmail.com>
2022-08-05 06:39:24 +0100

    print passed numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - functions that prints all numbers passed
+* @separator: pointer parameter
+* @n: passed parameter
+* Return: numbers passed
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_list(ap, int));
+		if (separator && i < n - 1)
+		{
+			printf("%s", seperator);
+		}
+	}
+	printf("\n");
+	va_end(valist);
+
+
+}
